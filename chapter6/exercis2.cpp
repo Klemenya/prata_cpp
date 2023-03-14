13b15e40de1cb9a66925ac2159efd32e9917c516
Klemenya <Klemenya@gmail.com>
2023-03-14 09:05:50 +0300

    end of exercise2.cpp

--- chapter6/exercis2.cpp
+++ chapter6/exercis2.cpp
@@ -9,9 +9,28 @@
 #include <iostream>
 
 int main(int argc, char ** argv){
-    char str[30] = "12.73645 kajdhf";
-    char *end;
-    double ld = atof(str);
-    std::cout << ld << std::endl;
+    const int SIZEARR = 10;
+    double arr[SIZEARR]{};
+    int i{};
+    double sum{};
+    std::cout << "Please, enter the number: \n";
+    std::cout << "Number 1#: ";
+    while (i < SIZEARR && std::cin >>arr[i] ){
+        sum+=arr[i];
+        ++i;
+        if(i < SIZEARR){
+            std::cout << "Number " << i+1 << "#: ";
+        }
+        
+    }
+    double averageValue = sum / i;
+    std::cout << "\nAverage value: " << averageValue << std::endl;
+
+    for(int j{}; j < i;++j){
+        if(arr[j]>averageValue){
+            std::cout << "this number " << arr[j] << " is greater than average value" << std::endl;
+        }
+    }
+
     return 0;
 }
